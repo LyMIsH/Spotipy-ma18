673fb0864c6759763fd2187acbf1edde370a57eb
Lymish <ronuthup@gmail.com>
2022-02-09 19:27:59 +0200

    Renamed console ui file

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import config
 from core.datahandling import search
 from consolemenu import *
 from consolemenu.items import *
-from core.uifunctionality import basefunctions
+from core.uifunctionality import uifunctions
 
 
 def main():
     login_menu = ConsoleMenu("Spotipy", "Login menu")
     main_menu = ConsoleMenu("Spotipy", "Main menu")
-    login_option = FunctionItem("Login", basefunctions.login_console, should_exit=True)
+    login_option = FunctionItem("Login", uifunctions.login_console, should_exit=True)
     login_menu.append_item(login_option)
     login_menu.show()
     user = login_option.return_value
-    create_playlist_option = FunctionItem("Create playlist", basefunctions.create_playlist_console, [user])
-    add_track_option = FunctionItem("Add track to playlist", basefunctions.add_track_console, [user])
+    create_playlist_option = FunctionItem("Create playlist", uifunctions.create_playlist_console, [user])
+    add_track_option = FunctionItem("Add track to playlist", uifunctions.add_track_console, [user])
     get_artists_option = FunctionItem("Get artists", print, search.get_artists())
-    get_albums_option = FunctionItem("Get album by artist ID", basefunctions.get_artist_albums_console)
-    get_album_tracks_option = FunctionItem("Get album tracks by ID", basefunctions.get_album_tracks_console)
-    get_top_tracks_option = FunctionItem("Get top 10 tracks by author ID", basefunctions.get_top_tracks_console)
+    get_albums_option = FunctionItem("Get album by artist ID", uifunctions.get_artist_albums_console)
+    get_album_tracks_option = FunctionItem("Get album tracks by ID", uifunctions.get_album_tracks_console)
+    get_top_tracks_option = FunctionItem("Get top 10 tracks by author ID", uifunctions.get_top_tracks_console)
     main_menu.append_item(create_playlist_option)
     main_menu.append_item(add_track_option)
     main_menu.append_item(get_artists_option)
