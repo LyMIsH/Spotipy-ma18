da6b2652bfcf0f708afd99b58a4aecd5165e69d4
Lymish <ronuthup@gmail.com>
2022-02-09 19:30:14 +0200

    Changed file name again

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import config
 from core.datahandling import search
 from consolemenu import *
 from consolemenu.items import *
-from core.uifunctionality import uifunctions
+from core.uifunctionality import consolefunctions
 
 
 def main():
     login_menu = ConsoleMenu("Spotipy", "Login menu")
     main_menu = ConsoleMenu("Spotipy", "Main menu")
-    login_option = FunctionItem("Login", uifunctions.login_console, should_exit=True)
+    login_option = FunctionItem("Login", consolefunctions.login_console, should_exit=True)
     login_menu.append_item(login_option)
     login_menu.show()
     user = login_option.return_value
-    create_playlist_option = FunctionItem("Create playlist", uifunctions.create_playlist_console, [user])
-    add_track_option = FunctionItem("Add track to playlist", uifunctions.add_track_console, [user])
+    create_playlist_option = FunctionItem("Create playlist", consolefunctions.create_playlist_console, [user])
+    add_track_option = FunctionItem("Add track to playlist", consolefunctions.add_track_console, [user])
     get_artists_option = FunctionItem("Get artists", print, search.get_artists())
-    get_albums_option = FunctionItem("Get album by artist ID", uifunctions.get_artist_albums_console)
-    get_album_tracks_option = FunctionItem("Get album tracks by ID", uifunctions.get_album_tracks_console)
-    get_top_tracks_option = FunctionItem("Get top 10 tracks by artist ID", uifunctions.get_top_tracks_console)
+    get_albums_option = FunctionItem("Get album by artist ID", consolefunctions.get_artist_albums_console)
+    get_album_tracks_option = FunctionItem("Get album tracks by ID", consolefunctions.get_album_tracks_console)
+    get_top_tracks_option = FunctionItem("Get top 10 tracks by artist ID", consolefunctions.get_top_tracks_console)
     main_menu.append_item(create_playlist_option)
     main_menu.append_item(add_track_option)
     main_menu.append_item(get_artists_option)
